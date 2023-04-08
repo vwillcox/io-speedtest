2cdb4e773cdd0f844681333e6b2b70825bfe30c4
Vincent Willcox <talking@talktech.info>
2023-04-08 15:35:57 +0100

    Better error handling of the Speedtest JSON output

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-import subprocess, json, apikey	
+# -*- coding: utf-8 -*-
 
+import subprocess, json, apikey	
 from Adafruit_IO import Client, Feed
 
 #Setup for Adafuit_IO
-
 ADAFRUIT_IO_KEY = apikey.ADAFRUIT_IO_KEY
 ADAFRUIT_IO_USERNAME = apikey.ADAFRUIT_IO_USERNAME
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-#Run Speedtest.net CLI and output as JSON data
-stdoutdata = subprocess.getoutput("speedtest -f json")
-results = json.loads(stdoutdata)
-try:
-   for key in results:
-      download = results["download"]["bandwidth"] 
-      upload = results["upload"]["bandwidth"]
-      pings = results["ping"]["high"]
-      jit = results["ping"]["jitter"]
-      lat = results["ping"]["latency"]
-      isp = results["isp"]
+def run_speedtest():
+   #Run Speedtest.net CLI and output as JSON data
+   stdoutdata = subprocess.getoutput("speedtest -f json")
+   try:
+      results = json.loads(stdoutdata)
+      return(results)
+   except ValueError:
+      results = "Error"
+      pass
+   return(results)
+
+def decode_upload():
+   download = results["download"]["bandwidth"] 
+   upload = results["upload"]["bandwidth"]
+   pings = results["ping"]["high"]
+   jit = results["ping"]["jitter"]
+   lat = results["ping"]["latency"]
+   isp = results["isp"]
    #Format Data into English units
    down = str(round(download / 125000, 2))
    up = str(round(upload / 125000, 2))
@@ -34,5 +41,11 @@ try:
    aio.send(ping.key, pings)
    aio.send(jitter.key, jit)
    aio.send(latency.key, lat)
-except ValueError:
-   print("Thats an error")
+
+if __name__ == '__main__':
+   results=run_speedtest()
+   if (results != 'Error'):
+      print("Running")
+      decode_upload()
+   else :
+      print("Error")
