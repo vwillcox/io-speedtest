3c15a9c1c3ea211b32151120ccbc808a5ad154e8
Vincent Willcox <talking@talktech.info>
2023-04-08 12:31:29 +0100

    Minor changes

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt install curl python3 python3-pip -y
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt install speedtest -y
 pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 speedtest
